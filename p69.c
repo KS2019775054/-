f42d032910820a9d2e57cbf8ff56eb5e85b963f9
KS2019775054 <48702226+KS2019775054@users.noreply.github.com>
2019-11-19 17:55:28 +0900

    Update p69.c

--- p69.c
+++ p69.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int mycomp(const void *p1,const void *p2)
+{
+	const int *a1 = ( const int *)p1;
+	const int *a2 = ( const int *)p2;
+	if(*a1 < *a2)
+		return -1;
+	else if (*a1 == *a2)
+		return 0;
+	else 
+		return 1;
+}
 
 int main()
 {
 	int *a;
-	int n,b;
-	int c,h,g,m;
-	h = g = 0;
-	a = (int *) malloc (sizeof(int) * 6000000);
+	a = (int *) malloc (sizeof(int) * 5500000);
+	int n,b,c,h,e;
+	c =  e = 0;
+	h = 1;
 	scanf("%d",&n);
-	for(int i = 0; i<n; i++)
+	for(int i = 0 ; i < n ; i ++)
 	{
 		scanf("%d",&b);
 		a[i] = b;
 	}
-	for(int i = 0; i<n; i++)
+	qsort(a,n,sizeof(int),mycomp);
+	
+	for(int i = 0; i < n-1; i++)
 	{
-		c = a[i];
-		if(a[i]!= 0)
+		if(a[i] == a[i+1])
+			h++;
+		else
+			h = 1;
+		if(c <= h)
 		{
-			for(int j = 0; j < n; j++)
-			{
-				if(a[j]==c)
-				{
-					h++;
-				}
-				
-				if(g<h)
-				{
-					g = h;
-					m = a[j];
-				}
-				if(a[j]==c)
-				{
-					a[j] = 0;
-				}
-			} 				
+			c = h;
+			e = a[i];
 		}
-		h = 0;
+	
 	}
-	printf("%d %d",m,g);
+	printf("%d %d",e,c);
+	
 }
