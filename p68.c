7ba4a11f0077fdd43480bee5724ca32187b62a83
KS2019775054 <48702226+KS2019775054@users.noreply.github.com>
2019-11-20 14:47:41 +0900

    Update p68.c

--- p68.c
+++ p68.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long int mycomp(const void *p1,const void *p2)
+int mycomp(const void *p1,const void *p2)
 {
-	long long int *a1 = ( long long int *)p1;
-	long long int *a2 = ( long long int *)p2;
+	int *a1 = ( int *)p1;
+	int *a2 = (  int *)p2;
 	if(*a1 < *a2)
 		return -1;
 	else if (*a1 == *a2)
@@ -17,26 +17,35 @@ long long int mycomp(const void *p1,const void *p2)
 
 int main()
 {
-	long long int *a,b,n;
 
-	a = (long long int *) malloc (sizeof(long long int) * 7000000);
+	int *a,n;
+
+	a = ( int *) malloc (sizeof( int) * 1500000);
 	
-	scanf("%lld",&n);
+	scanf("%d",&n);
 	
-	for(long long int i = 0; i<n; i++)
+	for( int i = 0; i<n; i++)
+		scanf(" %d",&a[i]);
+		
+	
+	
+	/*
+	0 1 2 3 4 5 6
+	1 2 3 4 5 6 7
+	
+	0 1 2 3 4 5 6 7
+	1 2 3 4 5 6 7 8
+	*/
+	qsort(a,n,sizeof(int),mycomp);
+
+	if(n%2==0)
 	{
-		scanf("%lld",&b);
-		a[i] = b;
+		if(a[(n-2)/2]+a[(n)/2] == 0)
+			printf("0.0");
+		else
+			printf("%.1f",(a[(n-2)/2]+a[(n)/2])/2.0);
 	}
-	
-	
-	qsort(a,n,sizeof(long long int),mycomp);
-
-	if(n%2==0)	
-		printf("%.1lf",(a[(n/2)-1]+a[(n)/2])/2.0);
 	else
-		printf("%lld",a[n/2]);
+		printf("%d",a[(n-1)/2]);
 	
 }
-
-
